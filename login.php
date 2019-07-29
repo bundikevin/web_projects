fc8f2567356a99b47791498fff449a302f6e6a47
kemab <bundikevinmakori@gmail.com>
2019-07-29 21:45:42 +0300

    login

--- login.php
+++ login.php
@@ -2,35 +2,38 @@
 <html>
 <head>
 	<title>Login Page</title>
-	 
+	<link rel="stylesheet" type="text/css" href="login.css ">
 	<link rel="stylesheet" type="text/css" href="bootstrap-4.3.1-dist/css/bootstrap.css">
 	
 </head>
 <body>
- <div class="container" style="background: yellow">
-	<div id="frm" class="form-group">
-		<div class="h1">Login</div>
-		<form action="process.php" method="POST" class="form-group" >
-			<p>
-				<label>Username:</label>
-				
-				<input type="text" id="user" name="user"/>
-			</p>
-			<p>
-				<label>Password:</label>
-				<input type="password" id="pass" name="pass"/>
-			</p>
-			<p>
-				<input  type="submit" class="btn btn-secondary" value="Login"/>
-				<a class="btn" href="">Not Registered?</a>
-			</p>
-           
-			<p>
-				<input  type="submit"  class="btn btn-primary" value="Forgot Password?"/>
-			</p>
-		</form>
-
-	</div>
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    School Management System
+  </a>
+</nav>
+ <div class="container" >
+ <div class="login-form">
+    <form action="/examples/actions/confirmation.php" method="post">
+        <h2 class="text-center">Log in</h2>       
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Username" required="required">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Password" required="required">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">Log in</button>
+        </div>
+        <div class="clearfix">
+            <label class="pull-left checkbox-inline"><input type="checkbox"> Remember me</label>
+            <a href="#" class="pull-right">Forgot Password?</a>
+        </div>        
+    </form>
+    <p class="text-center"><a href="#">Create an Account</a></p>
+</div>
+	 
  </div >	
 </body>
 </html>
